93b9b9d49f7c068bfdd83853645ebf1075b1d7ac
NaTaLiA7674 <130678658+NaTaLiA7674@users.noreply.github.com>
2023-10-12 16:24:12 -0500

    Modificación de servicios y controladores

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,3 @@
-export * from './asignacion-conductor.service';
 export * from './calificar-al-cliente.service';
 export * from './calificar-al-conductor.service';
-export * from './parada.service';
-export * from './solicitud-viaje.service';
 
